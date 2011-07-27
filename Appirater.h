995ce27233a1f9d78298a5130fc0d016dd964398
Jens-Uwe Mager <jum@anubis.han.de>
2011-07-27 17:43:18 +0200

    Ok, I have seen the box enough times, only make it appear on extended debug.

--- Appirater.h
+++ Appirater.h
@@ -121,7 +121,7 @@ extern NSString *const kAppiraterDeclinedToRate;
  'YES' will show the Appirater alert everytime. Useful for testing how your message
  looks and making sure the link to your app's review page works.
  */
-#ifdef DEBUG
+#ifdef XDEBUG
 #define APPIRATER_DEBUG				YES
 #else
 #define APPIRATER_DEBUG				NO
