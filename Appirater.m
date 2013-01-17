946469af33e3ae401071b405cd2b2ffca812f6b8
mrcmd <mrcmd>
2013-01-17 19:46:31 +0100

    show compiler warning if not compiled with ARC

--- Appirater.m
+++ Appirater.m
@@ -38,6 +38,10 @@
 #import <SystemConfiguration/SCNetworkReachability.h>
 #include <netinet/in.h>
 
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
 NSString *const kAppiraterFirstUseDate				= @"kAppiraterFirstUseDate";
 NSString *const kAppiraterUseCount					= @"kAppiraterUseCount";
 NSString *const kAppiraterSignificantEventCount		= @"kAppiraterSignificantEventCount";
