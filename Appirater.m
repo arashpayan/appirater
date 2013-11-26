6ec28acbc99a0dc5f718f423a8a07b3b2e7137b0
Sebastian Husche <sebastian@doo.net>
2013-11-26 11:14:55 +0100

    Fix iOS7 deprecated warning.

--- Appirater.m
+++ Appirater.m
@@ -459,7 +459,7 @@ static BOOL _modalOpen = false;
 			[self setModalOpen:YES];
 			//Temporarily use a black status bar to match the StoreKit view.
 			[self setStatusBarStyle:[UIApplication sharedApplication].statusBarStyle];
-			[[UIApplication sharedApplication]setStatusBarStyle:UIStatusBarStyleBlackOpaque animated:_usesAnimation];
+			[[UIApplication sharedApplication]setStatusBarStyle:UIStatusBarStyleLightContent animated:_usesAnimation];
 		}];
 	
 	//Use the standard openUrl method if StoreKit is unavailable.
