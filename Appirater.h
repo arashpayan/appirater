f2cce4777f357ca26974bee7833dcc9557fce75e
Arash Payan <arash@ara.sh>
2014-10-08 19:48:59 -0700

    Fixes #194 (Typo in doc for setOpenInAppStore)

--- Appirater.h
+++ Appirater.h
@@ -272,7 +272,7 @@ extern NSString *const kAppiraterReminderRequestDate;
 + (void)setUsesAnimation:(BOOL)animation;
 
 /*!
- If set to YES, Appirater will open App Store link (instead of SKStoreProductViewController on iOS 6). Default NO.
+ If set to YES, Appirater will open App Store link (instead of SKStoreProductViewController on iOS 6). Default YES.
  */
 + (void)setOpenInAppStore:(BOOL)openInAppStore;
 
