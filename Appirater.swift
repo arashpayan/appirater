4f5968ccf5fa7b4194a5f78b768a578a135ce560
howtojazz <swchowtojazz@gmail.com>
2016-01-10 15:23:34 +0900

    added license description in appirater.swift

--- Appirater.swift
+++ Appirater.swift
@@ -1,6 +1,32 @@
+/*
+This file is part of Appirater.
+
+Copyright (c) 2012, Arash Payan
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 //
 //  Appirater.swift
-//  pm10
 //
 //  Created by SeokWon Cheul on 2016. 1. 9..
 //  Copyright © 2016년 won cheulseok. All rights reserved.
