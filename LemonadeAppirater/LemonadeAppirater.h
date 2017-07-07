4b760fdca69cbd61c19e2e8cd59fda36bb94eda8
Bassem Youssef <bassem@lemonade.io>
2017-07-06 20:50:44 -0700

    Framework

---
+++ LemonadeAppirater/LemonadeAppirater.h
@@ -0,0 +1,19 @@
+//
+//  LemonadeAppirater.h
+//  LemonadeAppirater
+//
+//  Created by Bassem Youssef on 7/6/17.
+//  Copyright © 2017 Lemonade.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LemonadeAppirater.
+FOUNDATION_EXPORT double LemonadeAppiraterVersionNumber;
+
+//! Project version string for LemonadeAppirater.
+FOUNDATION_EXPORT const unsigned char LemonadeAppiraterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LemonadeAppirater/PublicHeader.h>
+
+
